8069ebb66765eada2e069929af474130c97b2ce6
jesse10930 <ronda.jesse@gmail.com>
2019-05-20 11:37:54 -0400

    Updates

---
+++ precache-manifest.32568fc55c3c6df82944aa083269ca2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f35b30a36b912908a66bf93ccb8ecd3b",
+    "url": "/StarWarsAPI/static/media/cloneTrooper.f35b30a3.jpg"
+  },
+  {
+    "revision": "cc291d75d68ebcf9158d",
+    "url": "/StarWarsAPI/static/js/runtime~main.6bd4563e.js"
+  },
+  {
+    "revision": "009b0d48bccc0a4cb30d",
+    "url": "/StarWarsAPI/static/js/main.9d48d799.chunk.js"
+  },
+  {
+    "revision": "9170b29fb88fea3d427c",
+    "url": "/StarWarsAPI/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "009b0d48bccc0a4cb30d",
+    "url": "/StarWarsAPI/static/css/main.ffbd5941.chunk.css"
+  },
+  {
+    "revision": "9170b29fb88fea3d427c",
+    "url": "/StarWarsAPI/static/css/2.eff47c01.chunk.css"
+  },
+  {
+    "revision": "1ddf058cda45a9f666aafa261343225a",
+    "url": "/StarWarsAPI/index.html"
+  }
+];
